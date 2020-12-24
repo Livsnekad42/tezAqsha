48b56a1fd64a8c8a450113b7a30e13b1012f9cd9
Artem <savelevartem5@gmail.com>
2020-12-24 14:17:05 +0700

    add modal windows, add authorization

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./src/**/*.scss')
-.pipe(sass().on('error', sass.logError))
+.pipe(sass().on('error', sass.logError)).pipe(autoprefixer()).pipe(cleanCSS())
 .pipe(gulp.dest('./public/css'))
 })
 
